4a5926f2d55b58f80b43b0fc3a92b35d25b2a8fb
phantom-rain <david.ocampo@outlook.com>
2026-09-15 23:51:42 -0500

    Update streamlit_app.py
    made changes to allow orders to come through

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,32 @@
 # Import python packages
 import streamlit as st
-import pandas as py
+import pandas as pd
 import os
 from snowflake.snowpark.functions import col
-import requests  
+import requests
 
 # Write directly to the app
 st.title(f"Customize your Smoothie! :cup_with_straw: {st.__version__}")
-st.write("""Choose the fruits you want in your custom Smoothie!"""
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
+pd_df = my_dataframe.to_pandas()
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients: ', my_dataframe, max_selections=5)
-
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+ingredients_list = st.multiselect('Choose up to 5 ingredients: ', pd_df['FRUIT_NAME'], max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on.lower()}")
@@ -41,7 +35,6 @@ if ingredients_list:
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         else:
             st.warning(f"Couldn't find nutrition info for {fruit_chosen}.")
-
 
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
                      values (?, ?)"""
